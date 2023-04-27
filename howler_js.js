e0e965a99417f566c53fd5f35e9770ad5a841be9
unknown <sebastian.bilz@gmail.com>
2023-04-27 12:59:49 +0200

    adds new song

--- howler_js.js
+++ howler_js.js
@@ -6,8 +6,17 @@ const syncBtn = document.querySelector('#sync');
 const deSyncBtn = document.querySelector('#de-sync');
 const loopToggleBtn = document.querySelector('#loopToggle');
 
+// get the URL parameters
+const urlParams = new URLSearchParams(window.location.search);
+
+// get the value of the "song" parameter
+const song = urlParams.get("song");
+
+// use the value of the song variable as needed
+console.log(song);
+
 let loadedSounds = 0;
-var goodbye_blue_sky
+var current_song
 let allPaused = false;
 let pausePositions = []
 let ids = []
@@ -19,6 +28,7 @@ const range_style = document.querySelector('.range');
 range.style.left = "0%";
 range.style.right = "0%";
 
+let init_done = false;
 
 var loop_left_perc = 0
 var loop_right_perc = 100
@@ -28,16 +38,16 @@ var isDragging = false; // variable to track if the user is dragging the timelin
 
 function syncTracks2() {
   // Calculate the maximum seek position among all the sprites
-  let maxSeekPos = Math.max(...ids.map(id => Math.abs(goodbye_blue_sky.seek(id))));
+  let maxSeekPos = Math.max(...ids.map(id => Math.abs(current_song.seek(id))));
 
   // Set the seek position of all sprites to the maximum seek position
   ids.forEach((id, index) => {
-    goodbye_blue_sky.seek(maxSeekPos, id);
+    current_song.seek(maxSeekPos, id);
   });
 
   // Play all the sprites from the maximum seek position
-  for (var spriteName in goodbye_blue_sky._sprite) {
-    let spriteData = goodbye_blue_sky._sprite[spriteName];
+  for (var spriteName in current_song._sprite) {
+    let spriteData = current_song._sprite[spriteName];
     let spriteOffset = spriteData[0];
     let spriteDuration = spriteData[1];
 
@@ -45,21 +55,21 @@ function syncTracks2() {
     let remainingDuration = (spriteOffset + spriteDuration) / 1000 - maxSeekPos;
 
     if (remainingDuration > 0) {
-      goodbye_blue_sky.play(spriteName);
+      current_song.play(spriteName);
     }
   }
 }
 
 function deSyncTracks() {
   if (ids.length > 0) {
-    var sprite_offset_ref = goodbye_blue_sky._sprite[Object.entries(goodbye_blue_sky._sprite)[0][0]][0];
-    const seek_pos_ref = goodbye_blue_sky.seek(ids[0]);
+    var sprite_offset_ref = current_song._sprite[Object.entries(current_song._sprite)[0][0]][0];
+    const seek_pos_ref = current_song.seek(ids[0]);
     const abs_pos_ref = seek_pos_ref - sprite_offset_ref;
 
 
-    goodbye_blue_sky.seek(abs_pos_ref + 1.5, ids[0]);
+    current_song.seek(abs_pos_ref + 1.5, ids[0]);
 
-    printTrackSyncStatus() 
+    printTrackSyncStatus()
   }
 }
 
@@ -67,10 +77,10 @@ function deSyncTracks() {
 function printTrackSyncStatus() {
   console.log("-------------------------------------");
 
-  Object.entries(goodbye_blue_sky._sprite).forEach(([spriteName, spriteOffset], index) => {
+  Object.entries(current_song._sprite).forEach(([spriteName, spriteOffset], index) => {
 
     let sprite_offset = spriteOffset[0] / 1000;
-    let seek_pos = goodbye_blue_sky.seek(ids[index]);
+    let seek_pos = current_song.seek(ids[index]);
     let abs_pos = seek_pos - sprite_offset;
 
     console.log(spriteName + ": abs pos " + abs_pos);
@@ -83,20 +93,20 @@ function printTrackSyncStatus() {
 }
 
 function syncTracks() {
-  var sprite_offset_ref = goodbye_blue_sky._sprite[Object.entries(goodbye_blue_sky._sprite)[0][0]][0];
-  const seek_pos_ref = goodbye_blue_sky.seek(ids[0]);
+  var sprite_offset_ref = current_song._sprite[Object.entries(current_song._sprite)[0][0]][0];
+  const seek_pos_ref = current_song.seek(ids[0]);
   const abs_pos_ref = seek_pos_ref - sprite_offset_ref;
   console.log("-------------------------------------");
 
-  Object.entries(goodbye_blue_sky._sprite).forEach(([spriteName, spriteOffset], index) => {
+  Object.entries(current_song._sprite).forEach(([spriteName, spriteOffset], index) => {
 
     let sprite_offset = spriteOffset[0] / 1000;
-    let seek_pos = goodbye_blue_sky.seek(ids[index]);
+    let seek_pos = current_song.seek(ids[index]);
     let abs_pos = seek_pos - sprite_offset;
 
     if (Math.abs(abs_pos_ref - abs_pos) > 0.03) {
       // console.log("Out of syc by: " + Math.abs(abs_pos_ref - abs_pos));
-      goodbye_blue_sky.seek(abs_pos_ref + sprite_offset, ids[index]);
+      current_song.seek(abs_pos_ref + sprite_offset, ids[index]);
       console.log("Sync " + spriteName + " by " + abs_pos_ref - abs_pos);
     }
     console.log("abs pos " + abs_pos);
@@ -136,11 +146,11 @@ rangeInput.forEach((input) => {
   input.addEventListener("input", (e) => {
     let minRange = parseInt(rangeInput[0].value);
     let maxRange = parseInt(rangeInput[1].value);
-    if (maxRange - minRange < rangeMin) {     
+    if (maxRange - minRange < rangeMin) {
       if (e.target.className === "min") {
-        rangeInput[0].value = maxRange - rangeMin;        
+        rangeInput[0].value = maxRange - rangeMin;
       } else {
-        rangeInput[1].value = minRange + rangeMin;        
+        rangeInput[1].value = minRange + rangeMin;
       }
     } else {
       range.style.left = (minRange / rangeInput[0].max) * 100 + "%";
@@ -160,53 +170,104 @@ function seekTo(howl, seekTime) {
   return true; // Return true to indicate success
 }
 
-
+function loadTrack(track_id) {
+  if (track_id == "song1") {
+    return new Howl({
+      preload: true,
+      html5: true,
+      buffer: 8192,
+      "src": [
+        "./audio/goodbye_blue_sky.webm",
+        "./audio/goodbye_blue_sky.mp3"
+      ],
+      "sprite": {
+        "goodbye_blue_sky_17-Click": [
+          0,
+          149748.0045351474
+        ],
+        "goodbye_blue_sky_17-Soprano-Alto": [
+          151000,
+          149748.0045351474
+        ],
+        "goodbye_blue_sky_17-Soprano-Bass": [
+          302000,
+          149748.0045351474
+        ],
+        "goodbye_blue_sky_17-Soprano-Soprano": [
+          453000,
+          149748.0045351474
+        ],
+        "goodbye_blue_sky_17-Soprano-Tenor": [
+          604000,
+          149748.0045351474
+        ]
+      },
+    });
+  } else if (track_id == "song2") {
+    return new Howl({
+      preload: true,
+      html5: true,
+      buffer: 8192,
+      "src": [
+        "jar_let_her_go_mashup.webm",
+        "jar_let_her_go_mashup.mp3"
+      ],
+      "sprite": {
+        "VC_JarLetHerGo_Alt1": [
+          0,
+          195240
+        ],
+        "VC_JarLetHerGo_Alt2": [
+          197000,
+          195240
+        ],
+        "VC_JarLetHerGo_Bass1": [
+          394000,
+          195240
+        ],
+        "VC_JarLetHerGo_Bass2": [
+          591000,
+          195240
+        ],
+        "VC_JarLetHerGo_Click": [
+          788000,
+          195240
+        ],
+        "VC_JarLetHerGo_Sopran1": [
+          985000,
+          195240
+        ],
+        "VC_JarLetHerGo_Sopran2": [
+          1182000,
+          195240
+        ],
+        "VC_JarLetHerGo_Tenor1": [
+          1379000,
+          195240
+        ],
+        "VC_JarLetHerGo_Tenor2": [
+          1576000,
+          195240
+        ]
+      },
+    });
+  }
+}
 
 function activateSounds() {
+  console.log("Activating sounds...");
 
   // Load all sounds
-  goodbye_blue_sky = new Howl({
-    preload: true,
-    html5: true,
-    buffer: 8192,
-    "src": [
-      "./audio/goodbye_blue_sky.webm",
-      "./audio/goodbye_blue_sky.mp3"
-    ],
-    "sprite": {
-      "goodbye_blue_sky_17-Click": [
-        0,
-        149748.0045351474
-      ],
-      "goodbye_blue_sky_17-Soprano-Alto": [
-        151000,
-        149748.0045351474
-      ],
-      "goodbye_blue_sky_17-Soprano-Bass": [
-        302000,
-        149748.0045351474
-      ],
-      "goodbye_blue_sky_17-Soprano-Soprano": [
-        453000,
-        149748.0045351474
-      ],
-      "goodbye_blue_sky_17-Soprano-Tenor": [
-        604000,
-        149748.0045351474
-      ]
-    },
+  current_song = loadTrack(song);
+
+  Object.entries(current_song._sprite).forEach(([spriteName, spriteOffset], index) => {
+    ids.push(current_song.play(spriteName));
   });
+
   activateBtn.disabled = true; // disable the button
   activateBtn.removeEventListener("click", activateSounds);
 
-  ids.push(goodbye_blue_sky.play("goodbye_blue_sky_17-Click"))
-  ids.push(goodbye_blue_sky.play("goodbye_blue_sky_17-Soprano-Alto"))
-  ids.push(goodbye_blue_sky.play("goodbye_blue_sky_17-Soprano-Bass"))
-  ids.push(goodbye_blue_sky.play("goodbye_blue_sky_17-Soprano-Soprano"))
-  ids.push(goodbye_blue_sky.play("goodbye_blue_sky_17-Soprano-Tenor"))
-  goodbye_blue_sky.stop()
-
-  const spriteNames = Object.keys(goodbye_blue_sky._sprite);
+  const spriteNames = Object.keys(current_song._sprite);
 
   // Add event listeners to the fader elements
   ids.forEach((sound, index) => {
@@ -231,7 +292,19 @@ function activateSounds() {
     // Add an event listener to the slider to update the player's volume
     volumeSlider.addEventListener('input', () => {
       const newVolume = parseFloat(volumeSlider.value);
-      goodbye_blue_sky.volume(newVolume, ids[index]);
+      current_song.volume(newVolume, ids[index]);
+    });
+
+
+    current_song.on('play', function (soundId) {
+      if (!init_done) {
+        current_song.stop(soundId);
+        current_song.stop();
+        allPaused = false;
+      } else {
+        console.log(soundId);
+        // current_song.play(soundId)
+      }
     });
 
 
@@ -241,8 +314,8 @@ function activateSounds() {
   var max_duration = 1e-6;
   var max_duration_idx = 0;
 
-  Object.keys(goodbye_blue_sky._sprite).forEach((spriteName, index) => {
-    const spriteDuration = goodbye_blue_sky._sprite[spriteName][1] / 1000;
+  Object.keys(current_song._sprite).forEach((spriteName, index) => {
+    const spriteDuration = current_song._sprite[spriteName][1] / 1000;
 
     if (spriteDuration > max_duration) {
       max_duration = spriteDuration;
@@ -250,77 +323,92 @@ function activateSounds() {
     }
   });
 
-// update timeline
-var loopId;
-goodbye_blue_sky.on('play', function () {
-  loopId = setInterval(function () {
+  // update timeline
+  var loopId;
+  current_song.on('play', function () {
+    if (init_done) {
+      loopId = setInterval(function () {
 
-    const seek_time_ref = goodbye_blue_sky.seek(ids[max_duration_idx]);
-    if (!isDragging) { // only update the timeline if the user is not dragging it
-      timeline.value = seek_time_ref * 100 / max_duration;
+        const seek_time_ref = current_song.seek(ids[max_duration_idx]);
+        if (!isDragging) { // only update the timeline if the user is not dragging it
+          timeline.value = seek_time_ref * 100 / max_duration;
+        }
+
+        // TODO: Check if files are playing, even though they shouldn't, Check if all required files are playing correctly
+        // // // sync sound files again
+        // // sounds.forEach((sound, index) => {
+        // //   // console.log("diff time: " + Math.abs(seek_time_ref - sound.seek()))
+        // //   if (Math.abs(seek_time_ref - sound.seek()) > 0.03) {
+        // //     sound.seek(seek_time_ref);
+        // //   }
+        // // });
+
+        // check for looping
+        if (!loop_enabled || isDragging) {
+          return;
+        }
+
+        const loop_right_abs = loop_right_perc * max_duration / 100
+        const loop_left_abs = loop_left_perc * max_duration / 100
+
+        if (seek_time_ref >= loop_right_abs) {
+          timeline.value = loop_left_abs
+
+          Object.entries(current_song._sprite).forEach(([spriteName, spriteOffset], index) => {
+            let sprite_offset = spriteOffset[0] / 1000;
+            // let seek_pos = current_song.seek(ids[index]);
+            // let abs_pos = seek_pos - sprite_offset;
+            const timeline_abs_value = loop_left_abs
+            current_song.seek(loop_left_abs, ids[index]);
+
+
+          });
+        }
+
+        // Check if the sprite is playing
+        // if (!allPaused) {
+        //   return
+        // }
+
+        // for (id in ids) {
+        //   if (!current_song.playing(id)) {
+        //     console.log('The ' + id + ' sprite is not currently playing.');
+        //   }
+        // }                                               
+
+      }, 1000);
     }
-
-    // TODO: Check if files are playing, even though they shouldn't, Check if all required files are playing correctly
-    // // // sync sound files again
-    // // sounds.forEach((sound, index) => {
-    // //   // console.log("diff time: " + Math.abs(seek_time_ref - sound.seek()))
-    // //   if (Math.abs(seek_time_ref - sound.seek()) > 0.03) {
-    // //     sound.seek(seek_time_ref);
-    // //   }
-    // // });
-
-    // check for looping
-    if (!loop_enabled || isDragging) {
-      return;
-    }
-
-    const loop_right_abs = loop_right_perc * max_duration / 100
-    const loop_left_abs = loop_left_perc * max_duration / 100
-
-    if (seek_time_ref >= loop_right_abs) {
-      timeline.value = loop_left_abs
-
-      Object.entries(goodbye_blue_sky._sprite).forEach(([spriteName, spriteOffset], index) => {
-        let sprite_offset = spriteOffset[0] / 1000;
-        // let seek_pos = goodbye_blue_sky.seek(ids[index]);
-        // let abs_pos = seek_pos - sprite_offset;
-        const timeline_abs_value = loop_left_abs
-        goodbye_blue_sky.seek(loop_left_abs, ids[index]);
-      });
-    }
-
-  }, 10);
-});
-
-
-timeline.addEventListener('mousedown', () => {
-  isDragging = true; // user started dragging the timeline
-});
-
-timeline.addEventListener('mouseup', () => {
-  isDragging = false; // user stopped dragging the timeline
-});
-
-timeline.addEventListener('input', () => {
-  const seekTime = (parseFloat(timeline.value) / 100) * max_duration; // between 0 and 100%
-  //timeline.value = sounds[0].seek() / max_duration;
-  // console.log("Seek time = " + seekTime);
-  // console.log("Max duration = " + max_duration);
-  // sounds.forEach((sound, index) => {
-  //   sound.seek(seekTime);
-  // });
-
-  // TODO: Create generic "SeekTo()" function
-  Object.entries(goodbye_blue_sky._sprite).forEach(([spriteName, spriteOffset], index) => {
-
-    let sprite_offset = spriteOffset[0] / 1000;
-    // let seek_pos = goodbye_blue_sky.seek(ids[index]);
-    // let abs_pos = seek_pos - sprite_offset;
-    // const timeline_abs_value = loop_left_abs
-    goodbye_blue_sky.seek(seekTime+sprite_offset, ids[index]);
   });
 
-});
+
+  timeline.addEventListener('mousedown', () => {
+    isDragging = true; // user started dragging the timeline
+  });
+
+  timeline.addEventListener('mouseup', () => {
+    isDragging = false; // user stopped dragging the timeline
+  });
+
+  timeline.addEventListener('input', () => {
+    const seekTime = (parseFloat(timeline.value) / 100) * max_duration; // between 0 and 100%
+    //timeline.value = sounds[0].seek() / max_duration;
+    // console.log("Seek time = " + seekTime);
+    // console.log("Max duration = " + max_duration);
+    // sounds.forEach((sound, index) => {
+    //   sound.seek(seekTime);
+    // });
+
+    // TODO: Create generic "SeekTo()" function
+    Object.entries(current_song._sprite).forEach(([spriteName, spriteOffset], index) => {
+
+      let sprite_offset = spriteOffset[0] / 1000;
+      // let seek_pos = current_song.seek(ids[index]);
+      // let abs_pos = seek_pos - sprite_offset;
+      // const timeline_abs_value = loop_left_abs
+      current_song.seek(seekTime + sprite_offset, ids[index]);
+    });
+
+  });
 
 
 }
@@ -330,46 +418,19 @@ activateBtn.addEventListener("click", activateSounds);
 
 // Add an event listener to the play button to start or pause the sound
 playBtn.addEventListener('click', () => {
-
-  // if (!goodbye_blue_sky) {
-  //   alert("Please activate sounds first!");
-  //   return;
-  // }
-
-  // if (Howler.ctx.state === 'suspended') {
-  //   Howler.ctx.resume();
-  // }
-  // if (Howler._muted) {
-  //   Howler.unmute();
-  // }
-  // if (Howler._muted) {
-  //   alert("Please unmute your browser to use this app.");
-  //   return;
-  // }
-  // if (Howler._autoSuspend) {
-  //   Howler._autoSuspend = false;
-  // }
-
-  // if (Howler._suspendTimer) {
-  //   clearTimeout(Howler._suspendTimer);
-  //   Howler._suspendTimer = null;
-  // }
-  // if (Howler.ctx && Howler.ctx.state === 'suspended') {
-  //   Howler.ctx.resume();
-  // }
-
+  init_done = true;
   if (!allPaused) {
     allPaused = true;
     document.getElementById("play").textContent = "Pause";
     ids.forEach((id, index) => {
-      goodbye_blue_sky.play(id)
+      current_song.play(id)
     });
     syncTracks();
   } else {
     document.getElementById("play").textContent = "Play";
     allPaused = false;
     ids.forEach((id, index) => {
-      goodbye_blue_sky.pause(id)
+      current_song.pause(id)
     });
     syncTracks();
   }
@@ -378,14 +439,14 @@ playBtn.addEventListener('click', () => {
 var speedSlider = document.getElementById('speed');
 speedSlider.addEventListener('input', function () {
   var speed = parseFloat(speedSlider.value);
-  goodbye_blue_sky.rate(speed);
+  current_song.rate(speed);
 });
 
 stopBtn.addEventListener('click', () => {
   document.getElementById("play").textContent = "Play";
   allPaused = false;
   ids.forEach((id, index) => {
-    goodbye_blue_sky.stop(id)
+    current_song.stop(id)
   });
   syncTracks();
 });
